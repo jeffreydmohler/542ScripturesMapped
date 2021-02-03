4868fa8c889b59d21b1496e07fad8475f7ca33d0
Jeffrey Mohler <56555424+jeffreydmohler@users.noreply.github.com>
2021-02-02 22:23:32 -0700

    up to part 6

--- js/scriptures.js
+++ js/scriptures.js
@@ -20,9 +20,19 @@ const Scriptures = (function () {
     /*-------------------------------------------------------------------
      *                      CONSTANTS
      */
+    const BOTTOM_PADDING = "<br/><br/>";
+    const CLASS_BOOKS = "books";
+    const CLASS_VOLUME = "volume";
+    const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
+    const DIV_SCRIPTURES = "scriptures";
+    const REQUEST_GET = "GET";
+    const REQUEST_STATUS_OK = 200;
+    const REQUEST_STATUS_ERROR = 400;
+    const TAG_HEADERS = "h5";
     const URL_BASE = "https://scriptures.byu.edu/";
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
+
 
     /*-------------------------------------------------------------------
      *                      PRIVATE VARIABLES
@@ -35,7 +45,14 @@ const Scriptures = (function () {
      */
     let ajax;
     let cacheBooks;
+    let htmlAnchor;
+    let htmlDiv;
+    let htmlElement;
+    let htmlLink;
+    let htmlHashLink;
     let init;
+    let navigateHome;
+    let onHashChanged;
 
     /*-------------------------------------------------------------------
      *                      PRIVATE METHODS
@@ -43,10 +60,10 @@ const Scriptures = (function () {
     ajax = function (url, successCallback, failureCallback) {
         let request = new XMLHttpRequest();
 
-        request.open("GET", url, true);
+        request.open(REQUEST_GET, url, true);
 
         request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
+            if (request.status >= REQUEST_STATUS_OK && request.status < REQUEST_STATUS_ERROR) {
                 let data = JSON.parse(request.response);
 
                 if (typeof successCallback === "function") {
@@ -81,6 +98,63 @@ const Scriptures = (function () {
         }
     };
 
+    htmlAnchor = function (volume) {
+        return `<a name="v${volume.id}" />`;
+    };
+    
+    htmlDiv = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+    
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+    
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+    
+        return `<div${idString}${classString}>${contentString}</div`;
+    };
+    
+    htmlElement = function (tagName, content) {
+        return `<${tagName}>${content}</${tagName}>`;
+    };
+    
+    htmlLink = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let hrefString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+    
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+    
+        if (parameters.href !== undefined) {
+            hrefString = ` class="${parameters.href}"`;
+        }
+    
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+    
+        return `<div${idString}${classString}${hrefString}>${contentString}</div`;
+    };
+    
+    htmlHashLink = function (hashArguments, content) {
+        return `<a href="javascript:void(0)" onclick="changeHash(${hashArguments})">${content}</a>`;
+    };
+
     init = function (callback) {
         let booksLoaded = false;
         let volumesLoaded = false;
@@ -103,11 +177,43 @@ const Scriptures = (function () {
         });
     };
 
+    navigateHome = function (volumeId) {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = 
+            "<div>Old Testament</div>" +
+            "<div>New Testament</div>" +
+            "<div>Book of Mormon</div>" + 
+            "<div>Doctrine and Covenants</div>" +
+            "<div>Pearl of Great Price</div>" + volumeId;
+    };
+
+    onHashChanged = function () {
+        let ids = [];
+
+        if (location.hash !== "" && location.hash.length > 1) {
+            ids = location.hash.slice(1).split(":");
+        }
+
+        if (ids.length <= 0) {
+            navigateHome();
+        } 
+        else if (ids.length === 1) {
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1)[0].id) {
+                navigateHome()
+            }
+            else {
+                navigateHome(volumeId);
+            }
+        }
+    };
+
     /*-------------------------------------------------------------------
      *                      PUBLIC API
      */
     return {
-        init
+        init,
+        onHashChanged
     };
 
 }());
