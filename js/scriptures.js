e581bc9cf2ac6104f778678615233bd73780680a
Jeffrey Mohler <56555424+jeffreydmohler@users.noreply.github.com>
2021-02-02 21:38:12 -0700

    initial commit.

---
+++ js/scriptures.js
@@ -0,0 +1,113 @@
+/*========================================================================
+ * FILE:    scriptures.js
+ * AUTHOR:  Jeffrey Mohler
+ * DATE:    Winter 2021
+ *
+ * DESCRIPTION: Front-end JavaScript code for the Scriptures, Mapped.
+ *              IS 542, Winter 2021, BYU.
+ */
+/*jslint
+    browser
+*/
+/*property
+    books, forEach, init, maxBookId, minBookId, onerror, onload, open, parse,
+    push, response, send, status
+*/
+
+const Scriptures = (function () {
+    "use strict";
+
+    /*-------------------------------------------------------------------
+     *                      CONSTANTS
+     */
+    const URL_BASE = "https://scriptures.byu.edu/";
+    const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
+    const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
+
+    /*-------------------------------------------------------------------
+     *                      PRIVATE VARIABLES
+     */
+    let books;
+    let volumes;
+
+    /*-------------------------------------------------------------------
+     *                      PRIVATE METHOD DECLARATIONS
+     */
+    let ajax;
+    let cacheBooks;
+    let init;
+
+    /*-------------------------------------------------------------------
+     *                      PRIVATE METHODS
+     */
+    ajax = function (url, successCallback, failureCallback) {
+        let request = new XMLHttpRequest();
+
+        request.open("GET", url, true);
+
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                let data = JSON.parse(request.response);
+
+                if (typeof successCallback === "function") {
+                    successCallback(data);
+                }
+            } else {
+                if (typeof failureCallback === "function") {
+                    failureCallback(request);
+                }
+            }
+        };
+
+        request.onerror = failureCallback;
+        request.send();
+    };
+
+    cacheBooks = function (callback) {
+        volumes.forEach(function (volume) {
+            let volumeBooks = [];
+            let bookId = volume.minBookId;
+
+            while (bookId <= volume.maxBookId) {
+                volumeBooks.push(books[bookId]);
+                bookId += 1;
+            }
+
+            volume.books = volumeBooks;
+        });
+
+        if (typeof callback === "function") {
+            callback();
+        }
+    };
+
+    init = function (callback) {
+        let booksLoaded = false;
+        let volumesLoaded = false;
+
+        ajax(URL_BOOKS, function (data) {
+            books = data;
+            booksLoaded = true;
+
+            if (volumesLoaded) {
+                cacheBooks(callback);
+            }
+        });
+        ajax(URL_VOLUMES, function (data) {
+            volumes = data;
+            volumesLoaded = true;
+
+            if (booksLoaded) {
+                cacheBooks(callback);
+            }
+        });
+    };
+
+    /*-------------------------------------------------------------------
+     *                      PUBLIC API
+     */
+    return {
+        init
+    };
+
+}());
